2e4d1c8d81afca3bcd57e8521be9330524df12b8
henriquearaujo91 <henrique_araujo91@outlook.com>
2019-06-15 15:56:27 -0300

    configurando o PocApplication para reconhecer os diretorios dos arquivos da aplicacao

--- poc/src/main/java/com/lab/poc/PocApplication.java
+++ poc/src/main/java/com/lab/poc/PocApplication.java
@@ -2,8 +2,13 @@ package com.lab.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.lab.poc" }, exclude = JpaRepositoriesAutoConfiguration.class)
+@EntityScan(basePackages = { "com.lab.poc.model" })
+@EnableJpaRepositories(basePackages = { "com.lab.poc.repository" })
 public class PocApplication {
 
 	public static void main(String[] args) {

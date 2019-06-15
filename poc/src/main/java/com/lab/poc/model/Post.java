52baa7c27200e97bb07e8a30a45cb82c6b5a2cbe
henriquearaujo91 <henrique_araujo91@outlook.com>
2019-06-15 15:55:31 -0300

    criando entidade

--- poc/src/main/java/com/lab/poc/model/Post.java
+++ poc/src/main/java/com/lab/poc/model/Post.java
@@ -4,17 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity(name = "tbl_post")
 public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
